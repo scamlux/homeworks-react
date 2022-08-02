e392e2e64f9905eda06f15393bd40fb724325865
scamlux <scamlux@gmail.com>
2022-08-02 09:22:38 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="App" style={{ textAlign: "center" }}>
+       <strong>WE ARE THE CHAMPIONS</strong>
+      I've paid my dues <br />
+      Time after time
+      <br />
+      I've done my sentence
+      <br />
+      But committed no crime
+      <br />
+      And bad mistakes
+      <br />
+      I've made a few
+      <br />
+      I've had my share of sand
+      <br />
+      Kicked in my face
+      <br />
+      But I've come through
+      <br />
+      And we mean to go on and on and on and on
+      <br />
+      We are the champions, my friends
+      <br />
+      And we'll keep on fighting till the end
+      <br />
+      We are the champions
+      <br />
+      We are the champions
+      <br />
+      No time for losers
+      <br />
+      'Cause we are the champions of the World
+      <br />
+      I've taken my bows
+      <br />
+      And my curtain calls
+      <br />
+      You brought me fame and fortune
+      <br />
+      And everything that goes with it
+      <br />
+      I thank you all
+      <br />
+      But it's been no bed of roses
+      <br />
+      No pleasure cruise
+      <br />
+      I consider it a challenge before
+      <br />
+      The human race
+      <br />
+      And I ain't gonna lose
+      <br />
+      And we mean to go on and on and on and on
+      <br />
+      We are the champions, my friends
+      <br />
+      And we'll keep on fighting till the end
+      <br />
+      We are the champions
+      <br />
+      We are the champions
+      <br />
+      No time for losers
+      <br />
+      'Cause we are the champions of the World
+      <br />
+      We are the champions, my friends
+      <br />
+      And we'll keep on fighting till the end
+      <br />
+      We are the champions
+      <br />
+      We are the champions
+      <br />
+      No time for losers
+      <br />
+      'Cause we are the champions of the World
+      <br />
+    </h1>
   );
 }
 
