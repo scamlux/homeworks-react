3975b085a3717c73b25bb4c27c063a33bb94a73a
scamlux <scamlux@gmail.com>
2022-08-10 08:31:00 +0500

    few chala elements

--- src/components/tab/tab.jsx
+++ src/components/tab/tab.jsx
@@ -21,44 +21,41 @@ const renderTabBar = (props, DefaultTabBar) => (
 const Tab = () => (
   <StickyContainer>
     <Tabs defaultActiveKey="1" renderTabBar={renderTabBar}>
-      <TabPane tab="Tab 1" key="1" style={{ height: "500px" }}>
-        Content of Tab Pane 1
+      <TabPane tab="Tab 1" key="1" style={{ height: 500 }}>
+        1-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 2" key="2" style={{ height: "500px" }}>
-        Content of Tab Pane 2
+      <TabPane tab="Tab 2" key="2" style={{ height: 500 }}>
+        2-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 3" key="3" style={{ height: "500px" }}>
-        Content of Tab Pane 3
+      <TabPane tab="Tab 3" key="3" style={{ height: 500 }}>
+        3-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 4" key="4" style={{ height: "500px" }}>
-        Content of Tab Pane 4
+      <TabPane tab="Tab 4" key="4" style={{ height: 500 }}>
+        4-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 5" key="5" style={{ height: "500px" }}>
-        Content of Tab Pane 5
+      <TabPane tab="Tab 5" key="5" style={{ height: 500 }}>
+        5-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 6" key="6" style={{ height: "500px" }}>
-        Content of Tab Pane 6
+      <TabPane tab="Tab 6" key="6" style={{ height: 500 }}>
+        6-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 7" key="7" style={{ height: "500px" }}>
-        Content of Tab Pane 7
+      <TabPane tab="Tab 7" key="7" style={{ height: 500 }}>
+        7-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 8" key="8" style={{ height: "500px" }}>
-        Content of Tab Pane 8
+      <TabPane tab="Tab 8" key="8" style={{ height: 500 }}>
+        8-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 9" key="9" style={{ height: "500px" }}>
-        Content of Tab Pane 9
+      <TabPane tab="Tab 9" key="9" style={{ height: 500 }}>
+        9-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 10" key="10" style={{ height: "500px" }}>
-        Content of Tab Pane 10
+      <TabPane tab="Tab 10" key="10" style={{ height: 500 }}>
+        10-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 11" key="11" style={{ height: "500px" }}>
-        Content of Tab Pane 11
+      <TabPane tab="Tab 11" key="11" style={{ height: 500 }}>
+        11-chi TABdan Salom!
       </TabPane>
-      <TabPane tab="Tab 12" key="12" style={{ height: "500px" }}>
-        Content of Tab Pane 12
-      </TabPane>
-      <TabPane tab="Tab 13" key="13" style={{ height: "500px" }}>
-        Content of Tab Pane 13
+      <TabPane tab="Tab 12" key="12" style={{ height: 500 }}>
+        12-chi TABdan Salom!
       </TabPane>
     </Tabs>
   </StickyContainer>
